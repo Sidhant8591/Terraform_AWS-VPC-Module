2358132d5a1b0a6afe5cb1c176583cc3daac2e0a
Sidhant <148280811+Sidhant8591@users.noreply.github.com>
2026-03-17 03:04:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "vpc" {
   source             = "./module/vpc"  
-  vpc_cidr           = var.vpc_cidr           # Pass vpc_cidr variable
-  organization_prefix = var.organization_prefix # Pass organization_prefix variable
-  enable_dns_support  = var.enable_dns_support  # Pass other variables as needed
+  vpc_cidr           = var.vpc_cidr           
+  organization_prefix = var.organization_prefix 
+  enable_dns_support  = var.enable_dns_support  
   enable_dns_hostnames = var.enable_dns_hostnames
   public_subnets      = var.public_subnets
   app_subnets         = var.app_subnets
